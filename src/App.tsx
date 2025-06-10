e1ee2fe4265fabf0b28a7dbbeaeca320f90a8872
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-06-10 11:02:11 +0530

    Add: App.tsx with minimal resorces to display that webpavk is working

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,13 @@ import reactIcon from '@assets/react.svg';
 
 import Counter from '@/components/Counter';
 
-// const x: number = "abcd";
-// alert(x);
-
 function App() {
-    console.log(image);
     return (
         <>
             <h1>Hello, World</h1>
             <Counter />
             <br />
-            <img src={reactIcon} alt="" />
-            <br />
-            <img src={image} alt="" />
+           <h1> Nikhil + React</h1>
         </>
     );
 }
